5cd6a0a3e6e081024de1d75958125da678bb237f
Adam Bundschuh <bundschuh.adam@gmail.com>
2021-04-27 19:10:04 -0400

    Module/lecture complete for ES5/ES6 comparison and javascript engines

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-let name = 'Adam';
-let hobbies;
-let height;
+// let name = 'Adam';
+// let hobbies;
+// let height;
 
-if (name === 'Adam') {
-    hobbies = ['Computers', 'Eating', 'Space travel', 'Space travel while eating'];
-    console.log(hobbies);
-    height = 12;
-}
+// if (name === 'Adam') {
+//     hobbies = ['Computers', 'Eating', 'Space travel', 'Space travel while eating'];
+//     console.log(hobbies);
+//     height = 12;
+// }
 
-function greet () {
-    let age = 30;
-    let name = 'Alex';
-    console.log(name, age, hobbies);
-}
+// function greet () {
+//     let age = 30;
+//     let name = 'Alex';
+//     console.log(name, age, hobbies);
+// }
 
-console.log(name, hobbies, height);
+// console.log(name, hobbies, height);
 
-greet();
+// greet();
 
 "use strict";
 
 const userName = 'Adam';
+ver undefined = 5;
 
 console.log(userName);
