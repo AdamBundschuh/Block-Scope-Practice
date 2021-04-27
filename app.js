8d7bed97d0dd9b5b29998efa4b200616c5a7e4bf
Adam Bundschuh <bundschuh.adam@gmail.com>
2021-04-27 17:42:28 -0400

    Session 01, basics

---
+++ app.js
@@ -0,0 +1,19 @@
+let name = 'Adam';
+let hobbies;
+
+if (name === 'Adam') {
+    hobbies = ['Computers', 'Eating', 'Space travel'];
+    var height = 12;
+    console.log(hobbies);
+}
+
+function greet () {
+    let age = 30;
+    let name = 'Alex';
+    var height = 12;
+    console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies, height);
+
+greet();
